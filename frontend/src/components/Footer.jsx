8956a1c77e3e30c9b72edc9d5af41bcc8d96811d
NavinduBinuwara <gpn.binuwara@gmail.com>
2024-12-30 21:45:28 +0530

    Add Styles To The Footer Section

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,18 +3,18 @@ import { assets } from '../assets/assets'
 
 const Footer = () => {
   return (
-    <div>
-        <div>
+    <div className='md:mx-10'>
+        <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm'>
             {/*--------------Left Section----------------- */}
             <div>
-               <img src={assets.logo} alt="" />
-               <p>Empowering your health journey with easy appointments, smart reminders, personalized health tips, and advanced diabetes risk predictions. Your well-being, our priority.</p>
+               <img className='mb-5 w-40' src={assets.logo} alt="" />
+               <p className='w-full md:w-2/3 text-gray-600 leading-6'>Empowering your health journey with easy appointments, smart reminders, personalized health tips, and advanced diabetes risk predictions. Your well-being, our priority.</p>
             </div>
 
            {/*--------------Center  Section----------------- */}
            <div>
-                <p>Hospital</p>
-                <ul>
+                <p className='text-xl font-medium mb-5'>Hospital</p>
+                <ul className='flex flex-col gap-2 text-gray-600'>
                    <li>Home</li>
                    <li>About Us</li>
                    <li>Contact US</li>
@@ -24,8 +24,8 @@ const Footer = () => {
 
               {/*--------------Right  Section----------------- */}
            <div>
-                <p>GET IN TOUCH</p>
-                <ul>
+                <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
+                <ul className='flex flex-col gap-2 text-gray-600'>
                     <li>+94 777144250</li>
                     <li>Prescriptohospital@gmail.com</li>
                 </ul>
@@ -34,7 +34,7 @@ const Footer = () => {
         {/*-----------Copyright Text------------- */}
         <div>
              <hr />
-             <p>Copyright 2024@ Prescripto - All Right Reserved.</p>
+             <p className='py-5 text-sm text-center'>Copyright 2024@ Prescripto - All Right Reserved.</p>
         </div>
     </div>
   )
