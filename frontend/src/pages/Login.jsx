6eb8991a92a49ae706a2f6c3cbbc608dbe3de509
NavinduBinuwara <gpn.binuwara@gmail.com>
2024-12-31 22:58:10 +0530

    Add Styles To the Login Page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -16,21 +16,28 @@ const Login = () => {
   return (
     <form className='min-h-[80vg] flex items-center'>
         <div className='flex flex-col gap-3 m-auto items-start p-8 min-w-[340px] sm:min-w-96 border rounded-xl text-zinc-600 text-sm shadow-lg'>
-          <p>{state === 'Sign Up' ? "Create Account" : "Login"}</p>
+          <p className='text-2xl font-semibold'>{state === 'Sign Up' ? "Create Account" : "Login"}</p>
           <p>Please {state === 'Sign Up' ? "Sign Up" : "log in"} to book appointment</p>
-          <div>
+          {
+            state === "Sign Up" &&   <div className='w-full'>
             <p>Full Name</p>
-            <input type="text" onChange={(e)=>setName(e.target.name)} value={name} required />
+            <input className='border border-zinc-300 rounded w-full p-2 mt-1' type="text" onChange={(e)=>setName(e.target.name)} value={name} required />
           </div>
-          <div>
+          }
+          <div className='w-full'>
             <p>Email</p>
-            <input type="email" onChange={(e)=>setName(e.target.name)} value={name} required />
+            <input className='border border-zinc-300 rounded w-full p-2 mt-1' type="email" onChange={(e)=>setEmail(e.target.name)} value={email} required />
           </div>
-          <div>
+          <div className='w-full'>
             <p>Password</p>
-            <input type="password" onChange={(e)=>setName(e.target.name)} value={name} required />
+            <input className='border border-zinc-300 rounded w-full p-2 mt-1' type="password" onChange={(e)=>setPassword(e.target.name)} value={password} required />
           </div>
-          <button>{state === 'Sign Up' ? "Create Account" : "Login"}</button>
+          <button className='bg-primary text-white w-full py-2 rounded-md text-base'>{state === 'Sign Up' ? "Create Account" : "Login"}</button>
+          {
+            state === "Sign Up"
+            ? <p>Already have an account? <span onClick={()=>setState('Login')} className='text-primary underline cursor-pointer'>Login here</span></p>
+            : <p>Create an new account? <span onClick={()=>setState('Sign Up')} className='text-primary underline cursor-pointer'>click here</span></p>
+          }
         </div>
     </form>
   )
