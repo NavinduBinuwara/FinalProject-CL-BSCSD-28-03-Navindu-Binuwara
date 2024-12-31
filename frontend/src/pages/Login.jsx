09bc5e0eddcea771066498106aab6f8d472bed10
NavinduBinuwara <gpn.binuwara@gmail.com>
2024-12-31 22:38:53 +0530

    Login Page Create Account And Login Form Development

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,10 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Login = () => {
+
+  const [state,setState] = useState('Sign Up')
+  
+  const [email,setEmail] = useState('')
+  const [password,setPassword] = useState('')
+  const [name,setName] = useState('')
+
+  const onSubmitHandler = async (event) => {
+      event.preventDefault()
+
+  }
+
   return (
-    <div>
-        
-    </div>
+    <form className='min-h-[80vg] flex items-center'>
+        <div className='flex flex-col gap-3 m-auto items-start p-8 min-w-[340px] sm:min-w-96 border rounded-xl text-zinc-600 text-sm shadow-lg'>
+          <p>{state === 'Sign Up' ? "Create Account" : "Login"}</p>
+          <p>Please {state === 'Sign Up' ? "Sign Up" : "log in"} to book appointment</p>
+          <div>
+            <p>Full Name</p>
+            <input type="text" onChange={(e)=>setName(e.target.name)} value={name} required />
+          </div>
+          <div>
+            <p>Email</p>
+            <input type="email" onChange={(e)=>setName(e.target.name)} value={name} required />
+          </div>
+          <div>
+            <p>Password</p>
+            <input type="password" onChange={(e)=>setName(e.target.name)} value={name} required />
+          </div>
+          <button>{state === 'Sign Up' ? "Create Account" : "Login"}</button>
+        </div>
+    </form>
   )
 }
 
