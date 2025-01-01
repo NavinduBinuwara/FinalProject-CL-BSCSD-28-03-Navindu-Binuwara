57254a2616cb9cae55ee3a95c5e50e9e4d169eb4
NavinduBinuwara <gpn.binuwara@gmail.com>
2025-01-01 22:32:38 +0530

    Admin Panel Header Development and Add styles

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -4,6 +4,7 @@ import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { useContext } from 'react';
 import { AdminContext } from './context/AdminContext';
+import Navbar from './components/Navbar';
 
 const App = () => {
 
@@ -12,8 +13,9 @@ const App = () => {
 
 
   return aToken ? (
-    <div>
+    <div className='bg-[#F8F9FD]'>
       <ToastContainer/>
+      <Navbar/>
     </div>
   ) : (
     <>
