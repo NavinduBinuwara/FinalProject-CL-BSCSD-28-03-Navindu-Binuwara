c12d04d57e10bc19dd9392c01aaf10de2d8ed990
NavinduBinuwara <gpn.binuwara@gmail.com>
2025-01-02 11:38:52 +0530

    Add Doctor Controller and Route function to the frontend

--- backend/controllers/doctorController.js
+++ backend/controllers/doctorController.js
@@ -14,4 +14,17 @@ const changeAvailablity = async (req,res) => {
     }
 }
 
-export {changeAvailablity}
+const doctorList = async (req,res) => {
+    try {
+        
+        const doctors = await doctorModel.find({}).select(['-password','-email'])
+
+        res.json({success:true,doctors})
+
+    } catch (error) {
+        console.log(error)
+        res.json({sucess:false,message:error.message})
+    }
+}
+
+export {changeAvailablity,doctorList}
