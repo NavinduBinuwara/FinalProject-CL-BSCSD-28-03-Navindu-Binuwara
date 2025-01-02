83ccfe5a28c39fe4d98de33af2f5238b86b10029
NavinduBinuwara <gpn.binuwara@gmail.com>
2025-01-02 10:30:48 +0530

    Add Doctor page and Dorctos list page development in admin panel

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -80,4 +80,17 @@ const loginAdmin = async (req,res) => {
     }
 }
 
-export {addDoctor,loginAdmin}
+// API to get all doctors list for admin panel
+const allDoctors = async (req,res) => {
+   try {
+
+      const doctors = await doctorModel.find({}).select('-password')
+      res.json({sucess:true,doctors})
+    
+   } catch (error) {
+        console.log(error)
+        res.json({sucess:false,message:error.message})
+   }
+}
+
+export {addDoctor,loginAdmin,allDoctors}
